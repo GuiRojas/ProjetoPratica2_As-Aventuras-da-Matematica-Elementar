e958f500995613d04160ee56efb9b3e29fc6aaeb
Guilherme Rojas <30930231+nodoya@users.noreply.github.com>
2018-03-20 08:32:56 -0300

    "rorraz commita"

--- Jogo/Jogo/Form1.cs
+++ Jogo/Jogo/Form1.cs
@@ -4,49 +4,241 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing.Text;
+using System.Reflection;
 
 namespace Jogo
-{   //TODO pegar os niveis do bd
-    public partial class frmJogo : Form
+{
+    public partial class Form1 : Form
     {
-        Background background;
+        ObjGame heroi = new ObjGame(1, 1, 32,32);
+        ObjGame npc = new ObjGame(100, 100, 30, 32);
+        Bitmap imgHeroi = new Bitmap("Z:/pratica profissional/pratica2/Jogo/heroi.png");
+        Bitmap imgNpc = new Bitmap("Z:/pratica profissional/pratica2/Jogo/npc.png");
 
-        
-        public frmJogo()
+        bool goup;
+        bool godown;
+        bool goleft;
+        bool goright;
+        bool ocupLeft = false;
+        bool ocupRight = false;
+        bool ocupUp = false;
+        bool ocupDown = false;
+
+        int speed = 3;
+
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void Jogo_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            background = new Background(1);// TODO pegar do bd a fase do usuario
-            background.carregarGame();
+
         }
 
-        private void tmrMain_Tick(object sender, EventArgs e)
+        private void checarAreas()
         {
-            background.tick(sender, e);
+            //todo checar todos os objs da classe OjbGame
+
+            
+
+
+            if ( heroi.X == 0)
+            {
+                ocupLeft = true;
+            }
+            else
+            {
+                ocupLeft = false;
+            }
+
+            if (heroi.X == this.Width - heroi.Width)
+            {
+                ocupRight = true;
+            }
+            else
+            {
+                ocupRight = false;
+            }
+
+            if (heroi.Y == 0)
+            {
+                ocupUp = true;
+            }
+            else
+            {
+                ocupUp = false;
+            }
+
+            if (heroi.Y == this.Height - heroi.Height)
+            {
+                ocupDown = true;
+            }
+            else
+            {
+                ocupDown = false;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+            }
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (goleft)
+            {
+                for(int i = 0; i < speed; i++)
+                {
+                    checarAreas();
+                    if (!ocupLeft)
+                        heroi.X --;
+                }
+            }
+
+            if (goright)
+            {
+                for (int i = 0; i < speed; i++)
+                {
+                    checarAreas();
+                    if (!ocupRight)
+                        heroi.X ++;
+                }
+            }
+
+            if (goup)
+            {
+                for (int i = 0; i < speed; i++)
+                {
+                    checarAreas();
+                    if (!ocupUp)
+                        heroi.Y --;
+                }
+            }
+
+            if (godown)
+            {
+                for (int i = 0; i < speed; i++)
+                {
+                    checarAreas();
+                    if (!ocupDown)
+                        heroi.Y ++;
+                }
+            }
             Invalidate();
         }
-        
-        private void frmJogo_Paint(object sender, PaintEventArgs e)
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            background.paint(sender, e);
+            e.Graphics.DrawImage(imgHeroi, heroi.X, heroi.Y, heroi.Width, heroi.Height);
+            e.Graphics.DrawImage(imgNpc, npc.X, npc.Y, npc.Width, npc.Height);
+        }
+    }
+
+    public class ObjGame
+    {
+        protected int x { set; get; }
+        protected int y { set; get; }
+        protected int width { set; get; }
+        protected int height { set; get; }
+
+        public ObjGame(int xInicial, int yInicial, int widthInicial, int heightInicial)
+        {
+            this.x = xInicial;
+            this.y = yInicial;
+            this.width = widthInicial;
+            this.height = heightInicial;
         }
 
-        private void frmJogo_KeyDown(object sender, KeyEventArgs e)
+        public int Height
         {
-            background.keyDown(sender, e);
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
         }
 
-        private void frmJogo_KeyUp(object sender, KeyEventArgs e)
+        public int Width
         {
-            background.keyUp(sender, e);
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
         }
     }
 }
--- Jogo/Jogo/Properties/Resources.Designer.cs
+++ Jogo/Jogo/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace Jogo.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap fundo1 {
-            get {
-                object obj = ResourceManager.GetObject("fundo1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
